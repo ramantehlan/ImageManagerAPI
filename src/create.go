3d7b44733295ce8d5ed6cf6769fe5568784a93db
raman tehlan <ramantehlan@gmail.com>
2019-04-25 03:30:50 +0530

    POST handler 'create' added

---
+++ src/create.go
@@ -0,0 +1,8 @@
+package main
+
+import "net/http"
+
+// CreateSingleImage is the response to POST when a image is uploaded
+func CreateSingleImage(output http.ResponseWriter, reader *http.Request) {
+
+}
