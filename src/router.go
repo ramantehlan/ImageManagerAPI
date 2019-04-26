f840e4ff6ee3d1df57128a431e787a7f23c5c21d
raman tehlan <ramantehlan@gmail.com>
2019-04-27 02:58:17 +0530

    Post updated

--- src/router.go
+++ src/router.go
@@ -9,7 +9,7 @@ var routes = Routes{
 	Route{"Index", "GET", "/v1", ReadIndex},
 	Route{"GetAllImages", "GET", "/v1/image/all", ReadAllImages},
 	Route{"GetSingleImage", "GET", "/v1/image/{id}", ReadSingleImage},
-	Route{"PostSingleImage", "POST", "/v1/image/{id}", CreateSingleImage},
+	Route{"PostSingleImage", "POST", "/v1/image/upload", CreateSingleImage},
 }
 
 // GetRouter is to get all the routers which this API will serve
