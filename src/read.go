6c6673cfb957385669eaf031f956a3cd62ee593f
raman tehlan <ramantehlan@gmail.com>
2019-04-27 03:02:28 +0530

    Database connection to read

--- src/read.go
+++ src/read.go
@@ -5,46 +5,46 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/json-iterator/go"
 )
-
-// To add json-iterator
-var json = jsoniter.ConfigCompatibleWithStandardLibrary
 
 // ReadIndex is the response while the index url is accessed.
 func ReadIndex(output http.ResponseWriter, reader *http.Request) {
 	fmt.Fprintln(output, "ImageManagerAPI v1.0")
+	Log("info", "Endpoint Hit: ReadIndex")
 }
 
 // ReadAllImages is the response to return all the images
 func ReadAllImages(output http.ResponseWriter, reader *http.Request) {
-	dummy := Images{
-		Image{
-			ID:        0,
-			Image:     "Img",
-			Thumbnail: "Thumbnail",
-			Caption:   "Caption",
-		},
-		Image{
-			ID:        1,
-			Image:     "Img",
-			Thumbnail: "Thumbnail",
-			Caption:   "Caption",
-		},
+	Log("info", "Endpoint Hit: ReadAllImages")
+	results, err := DB.Query("SELECT * FROM images")
+	ErrorHandler(err)
+
+	var images Images
+	for results.Next() {
+		var img Image
+		// for each row, scan the result into our image composite object
+		err = results.Scan(&img.ID, &img.Image, &img.Thumbnail, &img.Caption)
+		ErrorHandler(err)
+
+		// Append images to array
+		images = append(images, img)
 	}
 
+	defer results.Close()
 	output.Header().Set("Content-Type", "application/json")
 	output.WriteHeader(http.StatusOK)
-	json.NewEncoder(output).Encode(dummy)
-
-	Log("info", "Endpoint Hit: ReadAllImages")
+	JSON.NewEncoder(output).Encode(images)
 }
 
 // ReadSingleImage This is to return the single image
 func ReadSingleImage(output http.ResponseWriter, reader *http.Request) {
-	vars := mux.Vars(reader)
-	imageID := vars["id"]
-	fmt.Fprintf(output, "imageId: "+imageID)
-
 	Log("info", "Endpoint Hit: ReadSingleImage")
+	vars := mux.Vars(reader)
+	var img Image
+	err := DB.QueryRow("SELECT * FROM images where id = ?", vars["id"]).Scan(&img.ID, &img.Image, &img.Thumbnail, &img.Caption)
+	ErrorHandler(err)
+
+	output.Header().Set("Content-Type", "application/json")
+	output.WriteHeader(http.StatusOK)
+	JSON.NewEncoder(output).Encode(img)
 }
